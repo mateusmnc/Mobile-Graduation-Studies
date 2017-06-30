41f54544f610b251f5a937443cdbe97f21a312c0
Mateus Manica <mateus.manica@gmail.com>
2017-06-30 01:01:48 -0300

    started exercise 2, screen with app features

---
+++ CalculadoradeCombustivel/app/src/main/java/com/unisinos/m2/mateusmanica/calculadoradecombustivel/MainActivity.java
@@ -0,0 +1,13 @@
+package com.unisinos.m2.mateusmanica.calculadoradecombustivel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
